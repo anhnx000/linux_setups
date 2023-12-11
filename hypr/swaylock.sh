ab4ee558f0fba4c5073262d978bc65b3a95b4c2a
xuananh000 <xuananhbka@gmail.com>
2023-12-11 17:09:17 +0700

    update dotfiles

---
+++ hypr/swaylock.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+swaylock \
+	--color 000000 \
+	--indicator-caps-lock \
+	--indicator-radius 150 \
+	--indicator-thickness 10 \
+	--font "CaskaydiaCove Nerd Font" \
+	--ring-color ebdbb2 \
+	--key-hl-color 8ec07c \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--inside-ver-color 83a598 \
+	--ring-ver-color 458588 \
+	--inside-wrong-color fb4934 \
+	--ring-wrong-color cc241d \
+	--text-wrong-color fe8019 \
+	--text-color fabd2f \
+	--separator-color 00000000
